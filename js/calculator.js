06bef4ff3ecaaa8d6499f8e248e104d2d21fd40b
Yun <yxia@spartaglobal.com>
2018-07-01 18:07:45 +0100

    initial statement working

---
+++ js/calculator.js
@@ -0,0 +1,12 @@
+var settings= prompt("Which calculator do you what to use?");
+//1-Basic, 2-Advanced,3-BMI,4-Trip
+
+switch (settings) {
+  case "1" :
+    alert("hello")
+    break;
+  default:
+    alert("Please enter a valid code.")
+    break;
+
+}
